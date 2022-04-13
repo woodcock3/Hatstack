c76826c99ba05baf741b921e00d1c4ca1cf36c30
Paul <64870518+woodcock3@users.noreply.github.com>
2022-04-13 04:07:43 +0100

    Update tailwind.config.js

--- themes/hatstack/tailwind.config.js
+++ themes/hatstack/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: process.env.HUGO_ENVIRONMENT === "production",
-    content: "./layouts/**/*.html"],
+    content: ["./layouts/**/*.html"],
     safelist: [],
   },
   theme: {
