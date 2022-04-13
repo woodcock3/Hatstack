aba3e8fab6e4faf6731f3be0662515a2b7683228
Paul <64870518+woodcock3@users.noreply.github.com>
2022-04-13 16:06:39 +0100

    Update postcss.config.js

--- themes/hatstack/postcss.config.js
+++ themes/hatstack/postcss.config.js
@@ -1,13 +1,12 @@
 // postcss.config.js
 
-const autoprefixer = require('autoprefixer');
-const tailwind = require('tailwindcss');
-const postcssimport = require('postcss-import');
+const autoprefixer = require("autoprefixer")({});
+const path = require("path");
 
 module.exports = {
   plugins: [
-    postcssimport,
-    tailwind,
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [autoprefixer] : [])
+    require("postcss-import"),
+    require("tailwindcss")(path.resolve(__dirname) + '/tailwind.config.js'),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [autoprefixer] : []),
   ],
 };
